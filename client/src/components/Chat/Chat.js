baae4c8230994adc96c4f704d5317c8d2686646d
unknown <shivanshkumar166@gmail.com>
2020-09-23 15:15:27 +0530

    Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,16 @@
 import React,{useState,useEffect} from 'react';
 import queryString from 'query-string'
 import io from 'socket.io-client'
+import './Chat.css'
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
+
 let socket;
 const Chat = ({location}) =>{
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000'
     useEffect(()=>{
         const {name,room} = queryString.parse(location.search)
@@ -27,11 +33,29 @@ return ()=>{
     },[ENDPOINT,location.search])
 
 useEffect(()=>{
-    
-})
+    socket.on('message',(message)=>{
+setMessages([...messages,message])
+    })
+},[messages])
+const sendMessage = (event)=>{
+    event.preventDefault()
+    if(message)
+    {
+        socket.emit('sendMessage',message,()=>setMessage(''))
+    }
+}
+console.log(message,messages)
+
+//function for sending messages
 
     return (
-        <h3>Chat</h3>
+       <div className="outerContainer">
+           <div className="container">
+               <InfoBar room={room}/>
+<Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+           </div>
+
+       </div>
     )
 }
 export default Chat
